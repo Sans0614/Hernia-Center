9e054119d27084b963df3526860d70c7644c5fd9
Sans <rebound19960614@gmail.com>
2022-11-04 18:51:47 +0800

    20221104 project upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  //要執行回到最上方
+  $('#goto-top-fixed-button').on('click',function(){
+     $("html,body").animate(
+      {
+        scrollTop:0 //回到第一個區塊
+      },
+      500
+    );
+  })
+});
---
+++ js/swiper.js
@@ -0,0 +1,149 @@
+var swiper = new Swiper(".service-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".service-swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".service-swiper-button-next",
+    prevEl: ".service-swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
+
+
+var swiper = new Swiper(".industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".food-service-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".food-service-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".food-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".food-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".medicine-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".medicine-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".beauty-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".beauty-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".bathroom-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".bathroom-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".social-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  //loop: true,
+  pagination: {
+    el: ".social-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".nonprofit-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  //loop: true,
+  pagination: {
+    el: ".nonprofit-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".clothing-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  //loop: true,
+  pagination: {
+    el: ".clothing-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".consultant-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  //loop: true,
+  pagination: {
+    el: ".consultant-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".service-process-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".service-process-swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".service-process-button-next",
+    prevEl: ".service-process-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".our-team-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".our-team-swiper-pagination",
+    clickable: true,
+  },
+});
